bbf51b6cc6d604aa5cadc0ad3b55f6d631cf17b6
DarkDave <darkdave1185@gmail.com>
2020-07-22 15:57:38 -0700

    able to get server running and basic page requests

--- CrashExpress/index.js
+++ CrashExpress/index.js
@@ -1,16 +1,26 @@
 //CALL IN EXPRESS
 const express = require('express');
+//CALL IN PATH(to handle file paths)
+const path = require('path');
 //INITIALIZE VARIABLE APP W/ EXPRESS
 const app = express();
 //GET REQUEST=go to webpage '/'(ROUTE)
 // a = request
 // b = response
 //FUNCTION
-app.get('/', (a, b) => {
-    //.send = send response to browser
-    b.send('<h1>ITS WORKING!</h1>')
+//--app.get('/', (a, b) => {
+//.send = send response to browser
+//--b.send('<h1>ITS WORKING!</h1>')
+// .sendFILE(w/ PATH now) 
+// path.join = combine informaiton into directory name: public/index.html
+//--b.sendFile(path.join(__dirname, 'public', 'index.html'))
+//--});
+//FUNCTION w/ STATIC FOLDER
+// .use(middleware) = defines what path to use for webpage
+// express.static(middleware) = sets common location to get webpage
+// path.join = combine informaiton into directory name: public/index.html
+app.use(express.static(path.join(__dirname, 'public')));
 
-});
 //SET PORT NUMBER
 //(process.env.PORT=want to look at evironment variable called PORT)
 //(if server port not available will run on 5000)
