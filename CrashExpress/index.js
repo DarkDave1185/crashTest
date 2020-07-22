89e18334a0c8f5d1bb975fdfc6d3794992fb4557
DarkDave <darkdave1185@gmail.com>
2020-07-22 16:22:13 -0700

    able ot get JSON from POSTMAN

--- CrashExpress/index.js
+++ CrashExpress/index.js
@@ -4,12 +4,14 @@ const express = require('express');
 const path = require('path');
 //INITIALIZE VARIABLE APP W/ EXPRESS
 const app = express();
-//GET REQUEST=go to webpage '/'(ROUTE)
+
+//**ROUTES**//
+//GET REQUEST=go to webpage '/' and return
 // a = request
 // b = response
 //FUNCTION
 //--app.get('/', (a, b) => {
-//.send = send response to browser
+//.send = send response from browser
 //--b.send('<h1>ITS WORKING!</h1>')
 // .sendFILE(w/ PATH now) 
 // path.join = combine informaiton into directory name: public/index.html
@@ -20,6 +22,35 @@ const app = express();
 // express.static(middleware) = sets common location to get webpage
 // path.join = combine informaiton into directory name: public/index.html
 app.use(express.static(path.join(__dirname, 'public')));
+//hard coded array to show JSON return
+const members = [{
+        id: 1,
+        name: "Jon",
+        age: 30,
+        status: "alive"
+    },
+    {
+        id: 2,
+        name: "Don",
+        age: 15,
+        status: "alive"
+    },
+    {
+        id: 3,
+        name: "Ron",
+        age: 85,
+        status: "dead"
+    },
+];
+//**ROUTES - API**//
+//GET REQUEST=go to webpage '/' and return
+// c = request
+// d = response
+app.get('/api/members', (c, d) => {
+    //.json = send response from browser in json form member array
+    d.json(members);
+});
+
 
 //SET PORT NUMBER
 //(process.env.PORT=want to look at evironment variable called PORT)
